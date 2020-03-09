22024d6776e350fd6ececc2bae89591f55658894
MasterElrond <47697440+MasterElrond@users.noreply.github.com>
2020-03-08 20:12:28 -0500

    try catch

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ const numUrls = urls.length-1;
 var index = 0;
 
 http.createServer(function(request, response) {
-  if (index == numUrls) {
+  if (index >== numUrls) {
     index = 0;
   }
   else {
     index += 1;
   }
-  const url = urls[index];
+  let url;
+  try {
+    url = urls[index];
+  }
+  catch {
+    url = urls[0];
+  }
   response.writeHead(302, {'Location': url});
   response.end();
 }).listen(PORT);
