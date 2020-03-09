d54ff8958cbb929b23e23441ed0bbc321eba3272
MasterElrond <47697440+MasterElrond@users.noreply.github.com>
2020-03-09 11:34:14 -0500

    deleted

--- user-data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo apt update
-sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-git clone https://github.com/MasterElrond/rickroll
-node rickroll/app.js
